fc2667415e232c5a41020f372a95087bd8cf4503
ChairmanWangII <50027145+ChairmanWangII@users.noreply.github.com>
2024-06-06 00:17:55 +0800

    feat: partly merge album feature into new branch checkouted from main

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,6 @@
+// If you change the structure of any frontmatter, run `npm run astro build sync` or any else
+// command to sync frontmatter type.
+
 import { SITE } from "@/config";
 import { defineCollection, z } from "astro:content";
 import { slug } from "github-slugger";
@@ -27,4 +30,28 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const album = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      author: z.string().default(SITE.author),
+      pubDatetime: z.date(),
+      title: z.string(),
+      sciName: z.string(),
+      cnName: z.string(),
+      comName: z.string(),
+      url: z.string(),
+      mediaType: z.string(),
+      images: z.array(z.string()),
+      ogImage: image()
+        .refine(img => img.width >= 1200 && img.height >= 630, {
+          message: "OpenGraph image must be at least 1200 X 630 pixels!",
+        })
+        .or(z.string())
+        .optional(),
+      description: z.string(),
+      canonicalURL: z.string().optional(),
+    }),
+});
+
+export const collections = { blog, album };
